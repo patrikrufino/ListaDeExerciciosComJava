242bdf183328c5a9a9fd2244d7f0645d7784d293
patrikrufino <patrik.rufino@gmail.com>
2022-10-05 11:29:04 -0300

    Cria metodo para calcular area de um circulo

---
+++ src/EstruturaSequencial/Helpers/GeometryAreas.java
@@ -0,0 +1,9 @@
+package EstruturaSequencial.Helpers;
+
+public class GeometryAreas {
+    public static final float PI = 3.142f;
+
+    public static float circleArea (float raio) {
+        return (PI * (raio * raio));
+    }
+}

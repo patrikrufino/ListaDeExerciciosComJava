174c35e5576265ae83c88d3eaf82ba8783d3bedd
patrikrufino <patrik.rufino@gmail.com>
2022-10-06 18:58:26 -0300

    cria um método para calcular a área de quadrado.

--- src/EstruturaSequencial/Helpers/GeometryAreas.java
+++ src/EstruturaSequencial/Helpers/GeometryAreas.java
@@ -6,4 +6,8 @@ public class GeometryAreas {
     public static float circleArea (float raio) {
         return (PI * (raio * raio));
     }
+
+    public static float squareArea (float sideSquare) {
+        return (float) Math.pow(sideSquare, 2);
+    }
 }

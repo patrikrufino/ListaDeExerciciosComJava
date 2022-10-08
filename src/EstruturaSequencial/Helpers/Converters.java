c88da9d990e06570200f3d775ea10e31eb54d9cc
patrikrufino <patrik.rufino@gmail.com>
2022-10-08 17:19:14 -0300

    cria um método para converter graus Fahrenheit pra Celsius.

--- src/EstruturaSequencial/Helpers/Converters.java
+++ src/EstruturaSequencial/Helpers/Converters.java
@@ -4,4 +4,8 @@ public class Converters {
     public static float converterMetersToCentimeters (float m) {
         return m * 100;
     }
+
+    public static float fahrenheitToCelsius(float f) {
+        return 5 * ((f - 32 ) / 9);
+    }
 }
